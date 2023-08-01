c83fc4732934520f122d07a0b62585fa98ac1ec4
Usman Haider <leo14haider@gmail.com>
2023-08-02 03:35:17 +0500

    add links to featured articles

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -26,25 +26,6 @@ export function HomePage({ data, latestArticles }: HomePageProps) {
 
   return (
     <div className="space-y-20">
-      {/* Header */}
-      {/* {title && <Header centered title={title} description={overview} />} */}
-      {/* Showcase projects */}
-      {/* {showcaseProjects && showcaseProjects.length > 0 && (
-        <div className="mx-auto max-w-[100rem] rounded-md border">
-          {showcaseProjects.map((project, key) => {
-            const href = resolveHref(project._type, project.slug)
-
-            if (!href) {
-              return null
-            }
-            return (
-              <Link key={key} href={href}>
-                <ProjectListItem project={project} odd={key % 2} />
-              </Link>
-            )
-          })}
-        </div>
-      )} */}
       {featuredArticles && featuredArticles.length > 0 && (
         <div className="grid grid-cols-1 md:grid-cols-3">
           {featuredArticles.map((article, key) => {
@@ -58,14 +39,16 @@ export function HomePage({ data, latestArticles }: HomePageProps) {
             return (
               <div className="grid grid-cols-4 border md:grid-cols-1">
                 <div className="relative order-last col-span-1 h-200 md:order-first md:col-span-1 md:h-300">
-                  {imageUrl && (
-                    <Image
-                      src={imageUrl}
-                      alt=""
-                      fill={true}
-                      style={{ objectFit: 'cover' }}
-                    />
-                  )}
+                  <Link href={resolveHref('article', article.slug) as Url}>
+                    {imageUrl && (
+                      <Image
+                        src={imageUrl}
+                        alt=""
+                        fill={true}
+                        style={{ objectFit: 'cover' }}
+                      />
+                    )}
+                  </Link>
                 </div>
                 <div className="col-span-3 md:col-span-1">
                   <div className="flex w-full flex-grow flex-col bg-white p-5 text-left leading-5 text-black sm:order-2 sm:block sm:h-full sm:p-10">
@@ -73,12 +56,12 @@ export function HomePage({ data, latestArticles }: HomePageProps) {
                       {article.tags}
                     </div>
                     <h3 className="m-0 w-full font-sans text-xl font-black leading-6 text-black">
-                      <a
-                        href="/en/article/epvp8j/threads-is-all-the-worst-parts-of-twitter-and-instagram-in-one-very-bad-app"
+                      <Link
+                        href={resolveHref('article', article.slug) as Url}
                         className="hover:text-neutral-400 focus:text-neutral-400 cursor-pointer bg-transparent leading-6 no-underline"
                       >
                         {article.title}
-                      </a>
+                      </Link>
                     </h3>
                     <div className="mx-0 mb-0 mt-2 hidden w-full font-sans text-base font-normal leading-7 text-black md:block lg:text-base">
                       <CustomPortableText
--- components/shared/CoverArticle.tsx
+++ components/shared/CoverArticle.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { ArticlePayload } from 'types'
 import { CustomPortableText } from './CustomPortableText'
 import { PortableTextBlock } from 'sanity'
+import Link from 'next/link'
+import { resolveHref } from 'lib/sanity.links'
+import { Url } from 'next/dist/shared/lib/router/router'
 
 export default function CoverArticle({ article }: { article: ArticlePayload }) {
   const imageUrl =
@@ -10,9 +13,11 @@ export default function CoverArticle({ article }: { article: ArticlePayload }) {
   return (
     <div className="grid grid-cols-1 md:col-span-3 md:grid-cols-3">
       <div className="relative order-2 col-span-1 h-400 md:order-1 md:col-span-2 md:h-600">
-        {imageUrl && (
-          <Image src={imageUrl} alt="" fill style={{ objectFit: 'cover' }} />
-        )}
+        <Link href={resolveHref('article', article.slug) as Url}>
+          {imageUrl && (
+            <Image src={imageUrl} alt="" fill style={{ objectFit: 'cover' }} />
+          )}
+        </Link>
       </div>
       <div className="order-1 col-span-1 flex md:order-2">
         <div className="flex-grow flex-col justify-center border-white bg-black p-5 md:p-10 lg:flex-1">
@@ -26,19 +31,12 @@ export default function CoverArticle({ article }: { article: ArticlePayload }) {
             </a>
           </div>
           <h3
-            className="m-0 w-full font-sans text-3xl font-black leading-none"
+            className="m-0 w-full font-sans text-3xl font-black leading-none text-white"
             style={{ lineHeight: '41.6px' }}
           >
-            <a
-              href="/en/article/v7b7nm/juan-luna-lost-painting-philippines-art"
-              className="cursor-pointer bg-transparent text-white hover:text-white focus:text-white"
-              style={{
-                textDecoration: 'none',
-                lineHeight: '41.6px',
-              }}
-            >
+            <Link href={resolveHref('article', article.slug) as Url}>
               <h2>{article.title}</h2>
-            </a>
+            </Link>
           </h3>
           <div className="mx-0 mt-2 w-full font-sans text-base leading-7 text-white">
             <CustomPortableText
